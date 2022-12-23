3052f524247570d0fa96bf3f266de475a39291fc
Lucas Lourenço <102922249+LucasLourencoo@users.noreply.github.com>
2022-12-23 20:32:13 -0300

    Update Selenium.py

--- Códigos Úteis/Selenium.py
+++ Códigos Úteis/Selenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,5 +17,4 @@ from selenium.common.exceptions import (
 servico = Service(ChromeDriverManager().install())
 drive = webdriver.Chrome(service=servico)
 drive.maximize_window()
-
 marktime = WebDriverWait(drive, 90)
